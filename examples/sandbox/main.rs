84ee04ed2eebd0a715b9f3dd80b24e34334939e2
TehnoTheDragon <tehnothedragon@gmail.com>
2024-06-19 21:00:48 +0300

    playing with pointers

--- examples/sandbox/main.rs
+++ examples/sandbox/main.rs
@@ -1,7 +1,13 @@
-use avm_rs_memory::mem::Memory;
+use avm_rs_memory::{access_memory, mem::{create_memory, MemorySliceTrait}, share_memory, vmem::VirtualMemory};
 
 fn main() {
-    let mut memory = Memory::new(128);
-    memory.write_string(0, "hello!");
-    println!("{memory}");
+    let mut memory = create_memory(32);
+    let mut virtual_memory = VirtualMemory::new(share_memory!(memory));
+    virtual_memory.allocate(4);
+    let mut ptr1 = virtual_memory.allocate(4);
+    ptr1.write_u8(0, 0x41);
+    ptr1.write_u8(1, 0x41);
+    ptr1.write_u8(2, 0x41);
+    ptr1.write_u8(3, 0x41);
+    println!("{}", access_memory!(memory));
 }
