686d1b667baf5dd6cc775c67e4286b02e11ea504
TehnoTheDragon <tehnothedragon@gmail.com>
2024-06-19 18:35:25 +0300

    play memory

--- examples/sandbox/main.rs
+++ examples/sandbox/main.rs
@@ -1,3 +1,7 @@
+use avm_rs_memory::mem::Memory;
+
 fn main() {
-    println!("Hello, world!");
+    let mut memory = Memory::new(128);
+    memory.write_string(0, "hello!");
+    println!("{memory}");
 }
